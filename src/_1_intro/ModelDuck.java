11920525f5ec9af295e29b10e31f21fce52528ee
Gokul-sv <gokulksp@gmail.com>
2022-12-02 08:30:37 +0530

    Adding new duck type

---
+++ src/_1_intro/ModelDuck.java
@@ -0,0 +1,14 @@
+package _1_intro;
+
+public class ModelDuck extends Duck{
+
+    public ModelDuck () {
+        flyBehaviour = new FlyNoWay();
+        quackBehaviour = new Quack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a model duck");
+    }
+}
