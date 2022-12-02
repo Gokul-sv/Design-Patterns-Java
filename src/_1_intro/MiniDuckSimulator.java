70e94d733e97bcb2a017e8e3a39cd14ce1b70c13
Gokul-sv <gokulksp@gmail.com>
2022-12-02 08:32:33 +0530

    Updating Duck Simulator with dynamic behaviour setting

--- src/_1_intro/MiniDuckSimulator.java
+++ src/_1_intro/MiniDuckSimulator.java
@@ -2,8 +2,17 @@ package _1_intro;
 
 public class MiniDuckSimulator {
     public static void main(String[] args) {
+
+        // Testing duck
         Duck mallard = new MallardDuck();
-        mallard.performFly();
         mallard.performQuack();
+        mallard.performFly();
+
+        // Setting behaviour dynamically
+        Duck model = new ModelDuck();
+        model.performFly();
+        // changing behaviour at runtime using the setter
+        model.setFlyBehaviour(new FlyRocketPowered());
+        model.performFly();
     }
 }
