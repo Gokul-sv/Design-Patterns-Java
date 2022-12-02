0746f1f02cf1fae8b8126e4c75e77d80fe64d58e
Gokul-sv <gokulksp@gmail.com>
2022-12-02 08:30:23 +0530

    Adding new behaviours

---
+++ src/_1_intro/FlyRocketPowered.java
@@ -0,0 +1,8 @@
+package _1_intro;
+
+public class FlyRocketPowered implements FlyBehaviour{
+    @Override
+    public void fly() {
+        System.out.println("I am flying with a rocket!");
+    }
+}
